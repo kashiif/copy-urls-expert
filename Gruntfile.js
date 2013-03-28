f6cc2b36b0dca0358ac0f5d07ee461510f30b69a
Kashif Iqbal Khan <kashiif@gmail.com>
2013-03-28 11:24:04 +0500

    Updated install.rdf to use values from package.json

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,10 +23,24 @@ module.exports = function(grunt) {
 		src: 'src/install.rdf',
 		dest: distdir + 'install.rdf',
 		options: {
-		  replacements: [{
-			pattern: /\<em\:version\>.+\<\/em\:version\>/ig,
+		  replacements: [
+		  {
+			pattern: /\<em\:version\>.+\<\/em\:version\>/g,
 			replacement: "<em:version>" + pkg.version + "</em:version>"
-		  }]
+		  },
+		  {
+			pattern: /\<em\:creator\>.+\<\/em\:creator\>/g,
+			replacement: "<em:creator>" + pkg.author.name + "</em:creator>"
+		  },
+		  {
+			pattern: /\<em\:homepageURL\>.*\<\/em\:homepageURL\>/g,
+			replacement: "<em:homepageURL>" + pkg.homepage + "</em:homepageURL>"
+		  },		  
+		  {
+			pattern: /\<em\:description\>.*\<\/em\:description\>/g,
+			replacement: "<em:description>" + pkg.description + "</em:description>"
+		  }
+		  ]
 		}
 	  }
 	},
