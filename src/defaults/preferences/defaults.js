2d1cf7559cce138ffff0dbcbb7c3b80327da4677
Kashif Iqbal Khan <kashiif@gmail.com>
2013-09-27 23:06:29 +0500

    Added UI for preference filterduplicates

--- src/defaults/preferences/defaults.js
+++ src/defaults/preferences/defaults.js
@@ -1,5 +1,6 @@
 pref('extensions.copyurlsexpert.sortby', 'default');
 pref('extensions.copyurlsexpert.copyhistory', false);
 pref('extensions.copyurlsexpert.toolbaraction', 'active-win');
+pref('extensions.copyurlsexpert.filterduplicates', true);
 pref('extensions.copyurlsexpert.opentabdelaystepinmillisecs', 80);
 pref('extensions.copyurlsexpert.version', '');
