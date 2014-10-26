c4704b7221eaa0976ee68bba119df456c3365afc
Kashif Iqbal Khan <kashiif@gmail.com>
2014-10-26 18:27:37 +1100

    Fixed #10: Open selected urls

--- src/chrome/content/copyurlsexpert.js
+++ src/chrome/content/copyurlsexpert.js
@@ -280,7 +280,7 @@ var copyUrlsExpert = {
 				// selections must be continuous
 				break;
 			}
-		}		
+		}	
 		return entries;		
 	},
 	
@@ -450,11 +450,31 @@ var copyUrlsExpert = {
 					
 	},
 	
-	performCopySelectedUrls : function(tagName, entryExtractor) {
-		var entries = copyUrlsExpert._getEntriesFromSelection(tagName, entryExtractor);
-		copyUrlsExpert._copyEntriesToClipBoard(entries, copyUrlsExpert._prefService);
+	performOpenUrlsInSelection: function() {
+		var entries = copyUrlsExpert._getEntriesFromSelection('a', copyUrlsExpert.getEntryFromLink);
+
+		var urls = new Array(entries.length);
+
+		for (var i=0 ; i<urls.length ; i++) {
+			urls[i] = entries[i].url;
+		}
+
+		copyUrlsExpert._openAllUrls(urls);
+	},
+
+	performCopyUrlsInSelection: function() {
+		copyUrlsExpert._performCopyUrlsInSelection('a', copyUrlsExpert.getEntryFromLink);
 	},
 	
+	performCopyUrlsOfImagesInSelection: function() {
+		copyUrlsExpert._performCopyUrlsInSelection('img', copyUrlsExpert.getEntryFromImage);
+	},
+
+	_performCopyUrlsInSelection: function(tagName, entryExtractor) {
+		var entries = this._getEntriesFromSelection(tagName, entryExtractor);
+		copyUrlsExpert._copyEntriesToClipBoard(entries, copyUrlsExpert._prefService);
+	},
+
 	onContentContextMenuShowing: function(evt) {
 		if (evt.target.id == 'contentAreaContextMenu')
 		{
@@ -566,7 +586,7 @@ var copyUrlsExpert = {
 	},
 
 	/**
-	This function is called from Open Tabs Dialog
+	This function is called for 'Open Tabs from Clipboard' 
 	*/
 	openTabs: function () {
 		var sUrl = this._getClipboardText(),
@@ -586,6 +606,10 @@ var copyUrlsExpert = {
 			urls.push(newUrl);
 		}
 
+		return copyUrlsExpert._openAllUrls(urls);
+	},
+
+	_openAllUrls: function (urls) {
 		if (!urls.length) return true;
 
 		var _g = this._gBrowser();
