7fbc23cd7f2298be9535419ebdf431003b330ef4
Kashif Iqbal Khan <kashiif@gmail.com>
2016-03-20 09:41:17 +1100

    Stop putting homepage in install.rdf via GruntFile

--- Gruntfile.js
+++ Gruntfile.js
@@ -63,10 +63,12 @@ module.exports = function(grunt) {
             pattern: /\<em\:creator\>.+\<\/em\:creator\>/g,
             replacement: "<em:creator>" + pkg.author.name + "</em:creator>"
           },
+          /*
           {
             pattern: /\<em\:homepageURL\>.*\<\/em\:homepageURL\>/g,
             replacement: "<em:homepageURL>" + pkg.homepage + "</em:homepageURL>"
-          },		  
+          },
+          */
           {
             pattern: /\<em\:description\>.*\<\/em\:description\>/g,
             replacement: "<em:description>" + pkg.description + "</em:description>"
